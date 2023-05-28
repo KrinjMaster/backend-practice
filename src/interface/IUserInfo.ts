b722660dde5854a735b92285576203d87b6cd1c1
KrinjMaster <5opkasuka@gmail.com>
2023-05-28 20:04:01 +0300

    ssr + useOptimistic

--- src/interface/IUserInfo.ts
+++ src/interface/IUserInfo.ts
@@ -4,9 +4,13 @@ export interface IPost {
   body: string
   rating: number
 }
-
 export interface IUserInfo {
   username: string
   password: string
-  posts: IPost[]
+  posts: IPost[] | []
+}
+export interface Target {
+  username: HTMLInputElement
+  password1: HTMLInputElement
+  password2: HTMLInputElement
 }
